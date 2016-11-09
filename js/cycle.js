7cc4d19e7be8a2dc2823fc864e62501ca006e3b1
Matt Shirlaw <mshirlaw@yahoo.com>
2016-11-09 15:33:30 +1100

    first commit

---
+++ js/cycle.js
@@ -0,0 +1,24 @@
+(function(){
+	
+	window.onload = init;
+	const DURATION = 2000;
+	var imageCounter;
+	var img; 
+	
+	function init() {
+		imageCounter = 1;
+		img = document.getElementById('screen');
+		setInterval(changeImage, DURATION);
+	}
+	
+	function changeImage() {
+		if (imageCounter === 0) {
+			img.src = 'img/home.png';
+			imageCounter++;
+		} else if (imageCounter === 1) {
+			img.src = 'img/original.png';
+			imageCounter = 0;
+		}
+	}
+	
+})();
